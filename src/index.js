8a98824314185f81ff2e6ff38e89af64d6bb3916
Christa Hegedus <Christa.Hegedus1@gmail.com>
2016-12-04 15:37:15 -0500

    alternating between x and o

--- src/index.js
+++ src/index.js
@@ -2,8 +2,31 @@ import './index.html'
 import './styles/screen.sass'
 
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  let player = 'x'
+  const cells = document.querySelectorAll('td')
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', () => {
+      cells[i].textContent = player
+      if (player === 'x') {
+        player = 'o'
+      } else {
+        player = 'x'
+      }
+    })
+  }
 }
+
+// const refresh = (reload) => {
+//   document.querySelector('button')
+//   let reload = 'button'
+//   console.log('hi')
+//   refresh.createElement('button')
+//   refresh.addEventListener('click', () => {
+//     refresh.textContent = refresh
+//     refresh.appendChild('refresh')
+//     window.location.reload(true)
+//   })
+// }
 
 document.addEventListener('DOMContentLoaded', main)
 
